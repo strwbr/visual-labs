b5896ac819993688398d733e217270255ba169bb
strwbr <borisova25122001@gmail.com>
2022-10-26 02:25:27 +0400

    added moving boundaries

--- visual_lab_3/ChartsForm.Designer.cs
+++ visual_lab_3/ChartsForm.Designer.cs
@@ -29,57 +29,159 @@ namespace visual_lab_3
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.brightAmountChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.leftBoundaryTrack = new System.Windows.Forms.TrackBar();
+            this.leftBoundaryValueTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rightBoundaryTrack = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rightBoundaryValueTb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.brightAmountChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftBoundaryTrack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightBoundaryTrack)).BeginInit();
             this.SuspendLayout();
             // 
-            // chart1
+            // brightAmountChart
             // 
-            chartArea2.AxisX.Maximum = 255D;
-            chartArea2.AxisX.Minimum = 0D;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(16, 15);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(579, 369);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            chartArea1.AxisX.Interval = 50D;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisX.MajorTickMark.TickMarkStyle = System.Windows.Forms.DataVisualization.Charting.TickMarkStyle.AcrossAxis;
+            chartArea1.AxisX.Maximum = 256D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.ScaleView.SmallScrollMinSizeType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.Name = "ChartArea1";
+            this.brightAmountChart.ChartAreas.Add(chartArea1);
+            this.brightAmountChart.IsSoftShadows = false;
+            this.brightAmountChart.Location = new System.Drawing.Point(72, 12);
+            this.brightAmountChart.Name = "brightAmountChart";
+            this.brightAmountChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.brightAmountChart.Series.Add(series1);
+            this.brightAmountChart.Size = new System.Drawing.Size(320, 300);
+            this.brightAmountChart.TabIndex = 0;
+            this.brightAmountChart.Text = "chart1";
+            this.brightAmountChart.KeyDown += new System.Windows.Forms.KeyEventHandler(this.brightAmountChart_KeyDown);
+            this.brightAmountChart.KeyUp += new System.Windows.Forms.KeyEventHandler(this.brightAmountChart_KeyUp);
+            this.brightAmountChart.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BrightAmountChart_MouseDown);
+            this.brightAmountChart.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BrightAmountChart_MouseMove);
+            this.brightAmountChart.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BrightAmountChart_MouseUp);
             // 
-            // trackBar1
+            // leftBoundaryTrack
             // 
-            this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(66, 416);
-            this.trackBar1.Maximum = 255;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(376, 56);
-            this.trackBar1.TabIndex = 1;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.leftBoundaryTrack.LargeChange = 1;
+            this.leftBoundaryTrack.Location = new System.Drawing.Point(110, 317);
+            this.leftBoundaryTrack.Margin = new System.Windows.Forms.Padding(2);
+            this.leftBoundaryTrack.Maximum = 255;
+            this.leftBoundaryTrack.Name = "leftBoundaryTrack";
+            this.leftBoundaryTrack.Size = new System.Drawing.Size(282, 45);
+            this.leftBoundaryTrack.TabIndex = 1;
+            this.leftBoundaryTrack.TickFrequency = 10;
+            this.leftBoundaryTrack.ValueChanged += new System.EventHandler(this.LeftBoundaryTrack_ValueChanged);
+            // 
+            // leftBoundaryValueTb
+            // 
+            this.leftBoundaryValueTb.Location = new System.Drawing.Point(26, 318);
+            this.leftBoundaryValueTb.Name = "leftBoundaryValueTb";
+            this.leftBoundaryValueTb.ReadOnly = true;
+            this.leftBoundaryValueTb.Size = new System.Drawing.Size(79, 20);
+            this.leftBoundaryValueTb.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "L";
+            // 
+            // rightBoundaryTrack
+            // 
+            this.rightBoundaryTrack.LargeChange = 1;
+            this.rightBoundaryTrack.Location = new System.Drawing.Point(110, 366);
+            this.rightBoundaryTrack.Margin = new System.Windows.Forms.Padding(2);
+            this.rightBoundaryTrack.Maximum = 255;
+            this.rightBoundaryTrack.Name = "rightBoundaryTrack";
+            this.rightBoundaryTrack.Size = new System.Drawing.Size(282, 45);
+            this.rightBoundaryTrack.TabIndex = 4;
+            this.rightBoundaryTrack.TickFrequency = 10;
+            this.rightBoundaryTrack.Value = 255;
+            this.rightBoundaryTrack.ValueChanged += new System.EventHandler(this.RightBoundaryTrack_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "R";
+            // 
+            // rightBoundaryValueTb
+            // 
+            this.rightBoundaryValueTb.Location = new System.Drawing.Point(26, 370);
+            this.rightBoundaryValueTb.Name = "rightBoundaryValueTb";
+            this.rightBoundaryValueTb.ReadOnly = true;
+            this.rightBoundaryValueTb.Size = new System.Drawing.Size(79, 20);
+            this.rightBoundaryValueTb.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(418, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(467, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
             // 
             // ChartsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.chart1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rightBoundaryValueTb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rightBoundaryTrack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.leftBoundaryValueTb);
+            this.Controls.Add(this.leftBoundaryTrack);
+            this.Controls.Add(this.brightAmountChart);
             this.Name = "ChartsForm";
             this.Text = "Диаграммы";
             this.Load += new System.EventHandler(this.ChartsForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brightAmountChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftBoundaryTrack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightBoundaryTrack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,7 +189,15 @@ namespace visual_lab_3
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart brightAmountChart;
+        private System.Windows.Forms.TrackBar leftBoundaryTrack;
+        private System.Windows.Forms.TextBox leftBoundaryValueTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar rightBoundaryTrack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rightBoundaryValueTb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- visual_lab_3/ChartsForm.cs
+++ visual_lab_3/ChartsForm.cs
@@ -15,9 +15,13 @@ namespace visual_lab_3
     {
         // Количество различных яркостей в изображении
         int[] amountBright = new int[256];
-        StripLine leftBoundary = new StripLine();
-        StripLine rightBoundary = new StripLine();
+        //StripLine leftBoundary = new StripLine();
+        //StripLine rightBoundary = new StripLine();
+        StripLine[] boundaries = new StripLine[2];
 
+        bool moveLeftBoundary = false;
+        bool moveRightBoundary = false;
+        bool moveBoth = false;
 
         public ChartsForm()
         {
@@ -41,39 +45,140 @@ namespace visual_lab_3
             {
                 amountBright[val]++;
             }
+
+            for(int i = 0; i < boundaries.Length; i++)
+            {
+                boundaries[i] = new StripLine();
+                boundaries[i].BackColor = Color.Red;
+                boundaries[i].StripWidth = 2;
+            }
         }
 
         private void ChartsForm_Load(object sender, EventArgs e)
         {
             for (int i = 0; i < amountBright.Length; i++)
             {
-                chart1.Series[0].Points.AddXY(i, amountBright[i]);
+                brightAmountChart.Series[0].Points.AddXY(i, amountBright[i]);
             }
-            UpdateStripLine(out rightBoundary, GetLeftBoundaryPos());
-        }
 
-        private void UpdateStripLine(out StripLine stripLine, int x)
-        {
-            StripLine temp = new StripLine();
-            temp.BackColor = Color.Red;
-            temp.IntervalOffset = x;
-            temp.BorderWidth = 1;
-            stripLine = temp;
+            int leftBoundaryPos = GetLeftBoundaryPos();
+            boundaries[0] = UpdateBoundaryPos(boundaries[0], leftBoundaryPos);
+            SetBoundary(boundaries[0]);
+            leftBoundaryValueTb.Text = leftBoundaryPos.ToString();
 
-            chart1.ChartAreas[0].AxisX.StripLines.Add(rightBoundary);
-
-        }
-
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            UpdateStripLine(out rightBoundary, GetLeftBoundaryPos());
-
+            int rightBoundaryPos = GetRightBoundaryPos();
+            boundaries[1] = UpdateBoundaryPos(boundaries[1], rightBoundaryPos);
+            SetBoundary(boundaries[1]);
+            rightBoundaryValueTb.Text = rightBoundaryPos.ToString();
         }
 
         private int GetLeftBoundaryPos()
         {
-            return trackBar1.Value;
+            return leftBoundaryTrack.Value;
         }
 
+        private int GetRightBoundaryPos()
+        {
+            return rightBoundaryTrack.Value;
+        }
+
+        private void LeftBoundaryTrack_ValueChanged(object sender, EventArgs e)
+        {
+            int leftBoundaryPos = GetLeftBoundaryPos();
+            boundaries[0] = UpdateBoundaryPos(boundaries[0],leftBoundaryPos);
+            SetBoundaries();
+            leftBoundaryValueTb.Text = leftBoundaryPos.ToString();
+            
+            label3.Text = amountBright[leftBoundaryPos].ToString();
+        }
+
+        private void RightBoundaryTrack_ValueChanged(object sender, EventArgs e)
+        {
+            int rightBoundaryPos = GetRightBoundaryPos();
+            boundaries[1] = UpdateBoundaryPos(boundaries[1], rightBoundaryPos);
+            SetBoundaries();
+            rightBoundaryValueTb.Text = rightBoundaryPos.ToString();
+
+            label4.Text = amountBright[rightBoundaryPos].ToString();
+        }
+
+        private void SetBoundaries()
+        {
+            brightAmountChart.ChartAreas[0].AxisX.StripLines.Clear();
+            SetBoundary(boundaries[0]);
+            SetBoundary(boundaries[1]);
+        }
+
+        private StripLine UpdateBoundaryPos(StripLine boundary, int x)
+        {
+            boundary.IntervalOffset = x;
+            return boundary;
+        }
+
+        private void SetBoundary(StripLine boundary)
+        {
+            brightAmountChart.ChartAreas[0].AxisX.StripLines.Add(boundary);
+        }
+
+        private void BrightAmountChart_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x = (int)brightAmountChart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X);
+            int xL = (int)boundaries[0].IntervalOffset;
+            int xR = (int)boundaries[1].IntervalOffset;
+            moveLeftBoundary = (x > xL - 3 && x < xL + 3);
+            moveRightBoundary = (x > xR - 3 && x < xR + 3);
+
+            // TODO обработать вариант когда они очень близко друг к другу
+            label5.Text = $"left: {moveLeftBoundary}, right: {moveRightBoundary}, both: {moveBoth}";
+        }
+
+        private void BrightAmountChart_MouseUp(object sender, MouseEventArgs e)
+        {
+            moveLeftBoundary = false;
+            moveRightBoundary = false;
+            label5.Text = $"left: {moveLeftBoundary}, right: {moveRightBoundary}, both: {moveBoth}";
+
+        }
+
+        private void BrightAmountChart_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = (int)brightAmountChart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X);
+            if (moveBoth)
+            {
+                // REFACTOR
+                leftBoundaryTrack.Value = x;
+                rightBoundaryTrack.Value = x;
+            }
+            else
+            {
+                if (moveLeftBoundary)
+                {
+                    //boundaries[0] = UpdateBoundaryPos(boundaries[0], x);
+                    leftBoundaryTrack.Value = x;
+                }
+                if (moveRightBoundary)
+                {
+                    rightBoundaryTrack.Value = x;
+                    //boundaries[1] = UpdateBoundaryPos(boundaries[1], x);
+                }
+            }
+        }
+
+        private void brightAmountChart_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                moveBoth = true;
+            }
+            label5.Text = $"left: {moveLeftBoundary}, right: {moveRightBoundary}, both: {moveBoth}";
+
+        }
+
+        private void brightAmountChart_KeyUp(object sender, KeyEventArgs e)
+        {
+            moveBoth = false;
+            label5.Text = $"left: {moveLeftBoundary}, right: {moveRightBoundary}, both: {moveBoth}";
+
+        }
     }
 }
